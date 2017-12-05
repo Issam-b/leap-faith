39646b340999cc6ebe0d97c932f4f93812aa1228
Issam <assam.bj@gmail.com>
2017-12-05 18:10:01 +0200

    draw fingers position on page

--- Google chrome extension/js/popup.js
+++ Google chrome extension/js/popup.js
@@ -1,26 +1,34 @@
 console.log("LeapJS v" + Leap.version.full);
 var state = 'fetch';
+
 window.onkeypress = function(e) {
-    if (e.charCode == 32) { //Space bar
-        if (state == 'fetch') {
-            state = 'pausing';
+    if (e.charCode == 32) { // Space bar
+        if (state == 'Connected') {
+            controller.disconnect();
+            state = 'Disconnected';
         } else {
-            state = 'fetch';
+            controller.connect();
+            state = 'Connected';
         }
     }
 };
 
 // finger names mapping
 const finger_name = ["thumb", "index", "middle", "ring", "pinky"];
+
 // var for stats div in html
 var stats_div;
 var frameDatadiv;
 var handData;
 var pointableData;
 var gesturesData;
+var canvas;
+var ctx;
+var element;
 
 // for gestures, part
 var temp;
+
 // Run this after page fully loads
 window.onload = function () {
     stats_div = document.getElementById('stats');
@@ -37,72 +45,61 @@ window.onload = function () {
     handData = document.getElementById('handData');
     pointableData = document.getElementById('pointableData');
     gesturesData = document.getElementById('gesturesData');
+
+    element = document.getElementById("position");
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
 };
 
-var haveLoggedFrame = false;
-// create the leap controller instance
-var controller = new Leap.Controller({enableGestures: true});
+// create the leap controller instance with parameters
+var controller = new Leap.Controller( {
+    enableGestures: true
+});
+
 // run the leap loop, this will be running until disconnected
 controller.loop(function(frame) {
-    // if paused state exist the loop
-    if (state == 'paused') return;
-    // if pausing state, print frame data and change state to paused
-    if (state == 'pausing') {
-        //stats_div.innerHTML = "<p><b>PAUSED</b></p><div>"+ frame.dump() + "</div>";
-        state = 'paused';
-    } else {
-        //stats_div.innerHTML = "<p>Click Space bar to pause</p><div>"+ frame.dump() + "</div>";
-    }
-
-    // check if frame has been logged and log it
-    if (haveLoggedFrame == false && frame.hands[0]){
-        console.log(frame);
-        haveLoggedFrame = true;
-    }
 
     // some useful and clear stats
-    // Show number of hands and fingers detected
-    var frameString =
-        // "Frame ID: " + frame.id  + "<br />" +
-        // "Timestamp: " + frame.timestamp + " &micro;s<br />" +
-        "Hands: " + frame.hands.length + "<br />" +
-        "Fingers: " + frame.fingers.length + "<br />";
+    PrintPointablesCount(frame);
+    PrintHandData(frame);
+    PrintFingerData(frame);
+    PrintGestureData(frame);
 
-    frameDatadiv.innerHTML = frameString;
+    // draw marker position on screen
+    PositionMarker(frame);
 
-    // Display Hand object data
-    var handString = "";
-    if (frame.hands.length > 0) {
-        for (var i = 0; i < frame.hands.length; i++) {
-            var hand = frame.hands[i];
-            handString += "Hand: " + hand.type + "<br />";
-            handString += "Direction: " + hand.direction.toString() + "<br />";
-            handString += "Palm normal: " + hand.palmNormal.toString() + "<br />";
-            handString += "Palm position: " + hand.palmPosition.toString() + " mm<br />";
-            handString += "Palm velocity: " + hand.palmVelocity.toString() + " mm/s<br />";
-            handString += "Sphere center: " + hand.sphereCenter.toString() + " mm<br />";
-            handString += "Sphere radius: " + hand.sphereRadius.toFixed(1) + " mm<br /><br />";
-        }
-        handData.innerHTML = handString;
-    }
+    // print fingers positions and movement to canvas on screen
+    ShowFingersCanvas(frame);
 
-    // Display Pointable (finger) object data
-    var pointableString = "";
+});
+
+function ShowFingersCanvas(frame) {
+
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    frame.pointables.forEach(function(pointable) {
+        var position = pointable.stabilizedTipPosition;
+        var normalized = frame.interactionBox.normalizePoint(position);
+
+        var x = ctx.canvas.width * normalized[0];
+        var y = ctx.canvas.height * (1 - normalized[1]);
+
+        ctx.beginPath();
+        ctx.rect(x, y, 20, 20);
+        ctx.fill();
+    });
+}
+function PositionMarker(frame) {
     if (frame.pointables.length > 0) {
-        for (var i = 0; i < frame.pointables.length; i++) {
-            var pointable = frame.pointables[i];
+        var position = frame.pointables[0].stabilizedTipPosition;
+        var normalized = frame.interactionBox.normalizePoint(position);
 
-            pointableString += "Pointable ID: " + finger_name[pointable.type] + "<br />";
-            pointableString += "Belongs to hand with ID: " + pointable.handId + "<br />";
-            pointableString += "Length: " + pointable.length.toFixed(1) + " mm<br />";
-            pointableString += "Width: "  + pointable.width.toFixed(1) + " mm<br />";
-            pointableString += "Direction: " + pointable.direction.toString() + "<br />";
-            pointableString += "Tip position: " + pointable.tipPosition.toString() + " mm<br />";
-            pointableString += "Tip velocity: " + pointable.tipVelocity.toString() + " mm/s<br /><br />";
-        }
-        pointableData.innerHTML = pointableString;
+        element.style.left = window.innerWidth * normalized[0];
+        element.style.top = window.innerHeight * (1 - normalized[1]);
     }
+}
 
+function PrintGestureData(frame) {
     // Gestures stats
     var gesturesString = "";
     var gestures = frame.gestures;
@@ -125,14 +122,59 @@ controller.loop(function(frame) {
             hands + ",    Fingers: " + fingers + "<br />";
     });
 
-
-
     if(gesturesString != "")
         temp = gesturesString;
 
     gesturesData.innerHTML = temp;
+}
 
-});
+function PrintFingerData(frame) {
+    // Display Pointable (finger) object data
+    var pointableString = "";
+    if (frame.pointables.length > 0) {
+        for (var i = 0; i < frame.pointables.length; i++) {
+            var pointable = frame.pointables[i];
+
+            pointableString += "Pointable ID: " + finger_name[pointable.type] + "<br />";
+            pointableString += "Belongs to hand with ID: " + pointable.handId + "<br />";
+            pointableString += "Length: " + pointable.length.toFixed(1) + " mm<br />";
+            pointableString += "Width: "  + pointable.width.toFixed(1) + " mm<br />";
+            pointableString += "Direction: " + pointable.direction.toString() + "<br />";
+            pointableString += "Tip position: " + pointable.tipPosition.toString() + " mm<br />";
+            pointableString += "Tip velocity: " + pointable.tipVelocity.toString() + " mm/s<br /><br />";
+        }
+        pointableData.innerHTML = pointableString;
+    }
+}
+
+function PrintPointablesCount(frame) {
+    // Show number of hands and fingers detected
+    var frameString =
+        // "Frame ID: " + frame.id  + "<br />" +
+        // "Timestamp: " + frame.timestamp + " &micro;s<br />" +
+        "Hands: " + frame.hands.length + "<br />" +
+        "Fingers: " + frame.fingers.length + "<br />";
+
+    frameDatadiv.innerHTML = frameString;
+}
+
+function PrintHandData(frame) {
+    // Display Hand object data
+    var handString = "";
+    if (frame.hands.length > 0) {
+        for (var i = 0; i < frame.hands.length; i++) {
+            var hand = frame.hands[i];
+            handString += "Hand: " + hand.type + "<br />";
+            handString += "Direction: " + hand.direction.toString() + "<br />";
+            handString += "Palm normal: " + hand.palmNormal.toString() + "<br />";
+            handString += "Palm position: " + hand.palmPosition.toString() + " mm<br />";
+            handString += "Palm velocity: " + hand.palmVelocity.toString() + " mm/s<br />";
+            handString += "Sphere center: " + hand.sphereCenter.toString() + " mm<br />";
+            handString += "Sphere radius: " + hand.sphereRadius.toFixed(1) + " mm<br /><br />";
+        }
+        handData.innerHTML = handString;
+    }
+}
 
 controller.on('ready', function() {
     console.log("ready. Service version: " + controller.connection.protocol.serviceVersion);
@@ -172,6 +214,7 @@ controller.on('streamingStopped', function(deviceInfo) {
 controller.on('deviceConnected', function() {
     console.log("deviceConnected");
 });
+
 controller.on('deviceDisconnected', function() {
     console.log("deviceDisconnected");
 });
